b3ed45c2fb65c08875801ed59f23e71ca95eb3cb
CesarEmiliano79 <cesaremi12rjpz@gmail.com>
2026-08-12 00:02:06 -0600

    Estilo de la pagina

--- mobile_dev/scheduler/src/App.jsx
+++ mobile_dev/scheduler/src/App.jsx
@@ -1,45 +1,84 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test hot module replacement (HMR).
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+const schedule = {
+  title: "CS Courses for 2018-2019",
+  "couses": {
+    "F101": {
+      "id": "F101",
+      "meets": "MWF 11:00-11:50",
+      "title": "Computer Science: Concepts, Philosophy, and Connections"
+    },
+    "F110": {
+      "id": "F110",
+      "meets": "MWF 10:00-10:50",
+      "title": "Intro Programming for non-majors"
+    },
+    "F111": {
+      "id": "F111",
+      "meets": "MWF 10:00-10:50",
+      "title": "Fundamentals of Computer Programming I"
+    },
+    "W110": {
+      "id": "W110",
+      "meets": "MWF 10:00-10:50",
+      "title": "Fundamentals of Computer Programming I"
+    },
+    "S313": {
+      "id": "S313",
+      "meets": "TuTh 15:30-16:50",
+      "title": "Tangible Interaction Design and Learning"
+    },
+    "S314": {
+      "id": "S314",
+      "meets": "TuTh 9:00-10:50",
+      "title": "Tech & Human Interaction"
+    }
+  }
 };
+
+const Banner = ({ title }) => (
+  <header>
+    <h1>{title}</h1>
+  </header>
+);
+
+const terms = {
+  "F": "Fall",
+  "W": "Winter",
+  "S": "Spring"
+};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Couse = ({ courses }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{getCourseTerm(courses)} CS {getCourseNumber(courses)}</div>
+      <div className="card-text">{courses.title}</div>
+    </div>
+  </div>
+);
+
+const CouseList = ({ courses }) => (
+  <div className="course-list">
+    {Object.values(courses).map(course => (
+      <Couse key={course.id} courses={course} />
+    ))}
+  </div>
+);
+
+const App = () => (
+  <div className="container">
+    <Banner title={schedule.title} />
+    <CouseList courses={schedule.couses} />
+  </div>
+);
 
 export default App;
